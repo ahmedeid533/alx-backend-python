b4e97b13f4d55c5b1b910a0883582cb94768fa27
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-18 14:17:06 +0200

    Python async v4.1

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
 	""" Let's execute multiple coroutines at the same time with async  """
 	delays = []
 	tasks = []
