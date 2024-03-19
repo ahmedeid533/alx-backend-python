49af19ddeb2af25dec9d2acd115b22c398173827
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-19 23:04:56 +0200

    Python async version 2 v0.1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+""" Defines an async generator """
 import asyncio
 import random
 
 
 async def async_generator():
+	""" Loops 10 times, yielding a random number between 0 and 10 """
 	for _ in range(10):
 		await asyncio.sleep(1)
 		yield random.randint(0, 10)
